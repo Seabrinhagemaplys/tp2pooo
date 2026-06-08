71c5516e278d8de50feb120a116f7c724d10a862
Seabrinhagemaplys <olivrolegal@gmail.com>
2026-06-08 14:00:13 -0700

    atualizacao nos utils

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 def validar_string(string: str) -> bool:
     """
     Valida se uma string está vazia e se o objeto passado é uma instância de string
@@ -8,3 +10,13 @@ def validar_string(string: str) -> bool:
         return False
     
     return True
+
+class EnumCor(Enum):
+    BRANCO = "Branco"
+    PRETO = "Preto"
+
+class EnumCaracteres(Enum):
+    PEAO_BRANCO = "♙"
+    PEAO_PRETO = "♟"
+    CAVALO_BRANCO = "♘"
+    CAVALO_PRETO = "♞"
