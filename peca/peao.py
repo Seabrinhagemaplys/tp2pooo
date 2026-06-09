2473b0792ee772311f13716af458eef79ae948f3
Lucas Szuster <lucasacszuster@gmail.com>
2026-06-09 19:16:09 -0300

    Ataulizei a forma que a funão é feita

--- peca/peao.py
+++ peca/peao.py
@@ -14,7 +14,6 @@ class Peao(Peca):
         )
     
     def atualizar_lista_de_possiveis_coordenadas(self):
-        
         match self.cor:
             case ut.EnumCor.BRANCO:
                 self.atualizar_lista_de_possiveis_coordenadas_peao_branco()
@@ -25,26 +24,26 @@ class Peao(Peca):
             case _:
                 pass
 
-    def atualizar_lista_de_possiveis_coordenadas_peao_branco(self):
-        def tentar_adicionar(self, linha: int, coluna: int):
-            """
-            Adiciona coordenada à lista somente se for válida no tabuleiro.
-            """
-            try:
-                self.lista_de_posssiveis_movimentos.append(Coordenada(linha, coluna))
-            except Exception as e:
-                pass
+    def tentar_adicionar(self, linha: int, coluna: int):
+        """
+        Adiciona coordenada à lista somente se for válida no tabuleiro.
+        """
+        try:
+            self.lista_de_posssiveis_movimentos.append(Coordenada(linha, coluna))
+        except Exception as e:
+            pass
 
+    def atualizar_lista_de_possiveis_coordenadas_peao_branco(self):
         linha_atual = self.coordenada_atual.linha
         coluna_atual = self.coordenada_atual.coluna
         self.lista_de_posssiveis_movimentos = []
 
-        tentar_adicionar(linha_atual - 1, coluna_atual)
-        tentar_adicionar(linha_atual - 1, coluna_atual + 1)
-        tentar_adicionar(linha_atual - 1, coluna_atual - 1)
+        self.tentar_adicionar(linha_atual - 1, coluna_atual)
+        self.tentar_adicionar(linha_atual - 1, coluna_atual + 1)
+        self.tentar_adicionar(linha_atual - 1, coluna_atual - 1)
 
         if not self.ja_movimentou:
-            tentar_adicionar(linha_atual - 2, coluna_atual)
+            self.tentar_adicionar(linha_atual - 2, coluna_atual)
 
     def atualizar_lista_de_possiveis_coordenadas_peao_preto(self):
         linha_atual = self.coordenada_atual.linha
