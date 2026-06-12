bea74997af21bd9cb6f4221e4a1270604c81043a
Lucas Szuster <lucasacszuster@gmail.com>
2026-06-12 12:25:58 -0300

    Iniciei o método atualizar_lista_de_possiveis_coordenadas() em rainha

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from peca.cavalo import Cavalo
 import utils.utils as ut
 from coordenada.coordenada import Coordenada
  
-c = Cavalo(ut.EnumCor.BRANCO, Coordenada(4, 4))
+c = Cavalo(ut.EnumCor.BRANCO, Coordenada(7, 5))
 
-print(c.lista_de_posssiveis_movimentos)
+print(len(c.lista_de_posssiveis_movimentos))
 
 for i in range(8):
     for j in range(8):
--- peca/rainha.py
+++ peca/rainha.py
@@ -12,4 +12,60 @@ class Rainha(Peca):
         ) 
 
     def atualizar_lista_de_possiveis_coordenadas(self):
-        pass
+        def calcular_diagonais(linha, coluna):
+            def dentro_do_tabuleiro(linha: int, coluna: int):
+                """
+                Função helper que checa se um part linha e coluna está dentro do tabuleiro
+                """
+                if ((linha < 0) or (coluna < 0)):
+                    return False
+                if ((linha > 7) or (coluna > 7)):
+                    return False
+                
+                return True
+
+            def adicionar_posicao_mais_mais(linha: int, coluna: int):
+                """
+                Função helper que adiciona todas as coordenadas possíveis na diagonal +/+ utilizando recursividade
+                """
+                if not dentro_do_tabuleiro(linha, coluna):
+                    return
+
+                adicionar_posicao_mais_mais(linha + 1, coluna + 1)
+                self.tentar_adicionar(linha, coluna)
+
+            def adicionar_posicao_mais_menos(linha: int, coluna: int):
+                """
+                Função helper que adiciona todas as coordenadas possíveis na diagonal +/- utilizando recursividade
+                """
+                if not dentro_do_tabuleiro(linha, coluna):
+                    return
+
+                adicionar_posicao_mais_menos(linha + 1, coluna - 1)
+                self.tentar_adicionar(linha, coluna)
+
+            def adicionar_posicao_menos_mais(linha: int, coluna: int):
+                """
+                Função helper que adiciona todas as coordenadas possíveis na diagonal -/+ utilizando recursividade
+                """
+                if not dentro_do_tabuleiro(linha, coluna):
+                    return
+
+                adicionar_posicao_menos_mais(linha - 1, coluna + 1)
+                self.tentar_adicionar(linha, coluna)
+
+            def adicionar_posicao_menos_menos(linha: int, coluna: int):
+                """
+                Função helper que adiciona todas as coordenadas possíveis na diagonal -/- utilizando recursividade
+                """
+                if not dentro_do_tabuleiro(linha, coluna):
+                    return
+
+                adicionar_posicao_menos_menos(linha - 1, coluna - 1)
+                self.tentar_adicionar(linha, coluna)
+
+            adicionar_posicao_mais_mais(linha, coluna)
+            adicionar_posicao_mais_menos(linha, coluna)
+            adicionar_posicao_menos_mais(linha, coluna)
+            adicionar_posicao_menos_menos(linha, coluna)
+        
