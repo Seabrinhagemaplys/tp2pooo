0c5e76d2d72ab255742a9951a1d62497712b53fe
Lucas Szuster <lucasacszuster@gmail.com>
2026-06-15 18:38:03 -0300

    Atualizei peao

--- peca/peao.py
+++ peca/peao.py
@@ -17,6 +17,22 @@ class Peao(Peca):
             coordenada=coordenada_inicial,
             tabuleiro=tabuleiro
         )
+
+    def tentar_adicionar(self, linha: int, coluna: int):
+        # Tem que ser sobrecarregado pois como o peão não pode tomar em casas frontais, 
+        # a útlima condição é levemente diferente, sendo (peca_na_casa is None) e não (peca_na_casa is None or peca_na_casa.cor != self.cor)
+        try:
+            coordenada = Coordenada(linha, coluna)
+            peca_na_casa = self.tabuleiro.get_peca_na_posicao(coordenada)
+
+            if (
+                coordenada not in self.lista_de_posssiveis_movimentos
+                and coordenada != self.coordenada_atual
+                and (peca_na_casa is None)
+            ):
+                self.lista_de_posssiveis_movimentos.append(coordenada)
+        except Exception as e:
+            pass
     
     def atualizar_lista_de_possiveis_coordenadas(self):
         self.lista_de_posssiveis_movimentos = []
@@ -61,14 +77,6 @@ class Peao(Peca):
             case _:
                 pass
 
-    def atualizar_lista_de_coordenadas_para_tomada_branca(self, linha_atual: int, coluna_atual: int):
-        self.tentar_adicionar(linha_atual - 1, coluna_atual - 1)
-        self.tentar_adicionar(linha_atual - 1, coluna_atual + 1)
-
-    def atualizar_lista_de_coordenadas_para_tomada_preto(self, linha_atual: int, coluna_atual: int):
-        self.tentar_adicionar(linha_atual + 1, coluna_atual - 1)
-        self.tentar_adicionar(linha_atual + 1, coluna_atual + 1)
-
     def tentar_adicionar_tomada(self, linha, coluna):
         """
         Adiciona coordenada à lista somente se for válida no tabuleiro, específico para tomadas do peão
@@ -80,10 +88,16 @@ class Peao(Peca):
             if (
                 coordenada not in self.movimentos_para_tomadas
                 and coordenada != self.coordenada_atual
-                and (peca_na_casa is None or peca_na_casa.cor != self.cor)
+                and (peca_na_casa is not None and peca_na_casa.cor != self.cor)
             ):
                 self.movimentos_para_tomadas.append(coordenada)
         except Exception as e:
             pass
 
-        
+    def atualizar_lista_de_coordenadas_para_tomada_branca(self, linha_atual: int, coluna_atual: int):
+        self.tentar_adicionar_tomada(linha_atual - 1, coluna_atual - 1)
+        self.tentar_adicionar_tomada(linha_atual - 1, coluna_atual + 1)
+
+    def atualizar_lista_de_coordenadas_para_tomada_preto(self, linha_atual: int, coluna_atual: int):
+        self.tentar_adicionar_tomada(linha_atual + 1, coluna_atual - 1)
+        self.tentar_adicionar_tomada(linha_atual + 1, coluna_atual + 1)    
