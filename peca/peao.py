63b114607114f10b260441182b7fa64f3821ea64
Lucas Szuster <lucasacszuster@gmail.com>
2026-06-15 18:39:12 -0300

    Atualizei peao

--- peca/peao.py
+++ peca/peao.py
@@ -35,29 +35,25 @@ class Peao(Peca):
             pass
     
     def atualizar_lista_de_possiveis_coordenadas(self):
+        linha_atual: int = self.coordenada_atual.linha
+        coluna_atual: int = self.coordenada_atual.coluna
         self.lista_de_posssiveis_movimentos = []
         match self.cor:
             case ut.EnumCor.BRANCO:
-                self.atualizar_lista_de_possiveis_coordenadas_peao_branco()
+                self.atualizar_lista_de_possiveis_coordenadas_peao_branco(linha_atual, coluna_atual)
             
             case ut.EnumCor.PRETO:
-                self.atualizar_lista_de_possiveis_coordenadas_peao_preto()
+                self.atualizar_lista_de_possiveis_coordenadas_peao_preto(linha_atual, coluna_atual)
             
             case _:
                 pass
 
-    def atualizar_lista_de_possiveis_coordenadas_peao_branco(self):
-        linha_atual: int = self.coordenada_atual.linha
-        coluna_atual: int = self.coordenada_atual.coluna
-
+    def atualizar_lista_de_possiveis_coordenadas_peao_branco(self, linha_atual, coluna_atual):
         self.tentar_adicionar(linha_atual - 1, coluna_atual)
         if not self.ja_movimentou:
             self.tentar_adicionar(linha_atual - 2, coluna_atual)
 
-    def atualizar_lista_de_possiveis_coordenadas_peao_preto(self):
-        linha_atual: int = self.coordenada_atual.linha
-        coluna_atual: int = self.coordenada_atual.coluna
-
+    def atualizar_lista_de_possiveis_coordenadas_peao_preto(self, linha_atual, coluna_atual):
         self.tentar_adicionar(linha_atual + 1, coluna_atual)
         if not self.ja_movimentou:
             self.tentar_adicionar(linha_atual + 2, coluna_atual)
